b7cbce25b87890aefe2d799534d36365d07dd18c
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-01-27 21:23:15 +0300

    refactor: added dialogs and optimized content block

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Profile from './Components/Profile/Profile';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 
 const App = () => {
@@ -15,7 +16,10 @@ const App = () => {
 
       <Navbar />
 
-      <Profile />
+      <div className='appWrapper_content'>
+        <Dialogs />
+      </div>
+      {/* <Profile /> */}
 
     </div>
     
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ import '../../reset.css'
 
     return(
 
-        <div className= {s.profileWrapper}> 
+        <div> 
             
         <div className={s.container}>
             <MainImage />
