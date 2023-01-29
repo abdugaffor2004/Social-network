b79d21d5324b00d7217356ff083dfea32d1076aa
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-01-29 20:47:21 +0300

    feat: rendered Dialogs page (without logic)

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -5,8 +5,38 @@ const Dialogs = (props) => {
 
     return(
 
-        <div>
-            Dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <h2>DIALOGS</h2>
+                {/* <div className={s.dialog}> Dilshod </div>
+                <div className={s.dialog}> Muhammed </div>
+                <div className={s.dialog}> Iskandar </div>
+                <div className={s.dialog}> Zafar </div>
+                <div className={s.dialog}> Shukur </div> */}
+
+                <ul>
+                    <li className={s.dialog}>Dilshod</li>
+                    <li className={s.dialog}>Muhammed</li>
+                    <li className={s.dialog}>Iskandar</li>
+                    <li className={s.dialog}>Zafar</li>
+                    <li className={s.dialog}>Shukur</li>
+                </ul>
+
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hey, yo!
+                </div>
+
+                <div className={s.message}>
+                    Why anybody don't love me?
+                </div>
+
+                <div className={s.message}>
+                    Inna Lillahi, Ina illahi Radzhiun
+                </div>
+            </div>
         </div>
 
     )
