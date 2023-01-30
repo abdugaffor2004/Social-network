e0b1e2f1539c6a18a5b3d8533d770cf47d5cd841
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-01-30 17:46:42 +0300

    refactor: Took the data aside (practiced)

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -5,23 +5,39 @@ import Messages from './Messages/Messages'
 
 const Dialogs = (props) => {
 
+    let dialogsData = [
+        {id: 1 , name:"Dilshod"},
+        {id: 2 , name:"Muhammed"},
+        {id: 3 , name:"Iskandar"},
+        {id: 4 , name:"Zafar"},
+        {id: 5 , name:"Shukur"}
+    ]
+
+    let messagesData = [
+        {id: 1 , message:"How is your react learning?"},
+        {id: 2 , message:"Hey, yo!"},
+        {id: 3 , message:"Inna Lillahi, Ina illahi Radzhiun"}
+        
+    ]
+
+
     return(
 
         <div className={s.dialogs}>
             
             <div className={s.dialogsItem}>
                 <h2>DIALOGS</h2>
-                <DialogsItem name='Dilshod' id='1' />
-                <DialogsItem name='Muhammed' id='2' />
-                <DialogsItem name='Iskandar' id='3' />
-                <DialogsItem name='Zafar' id='4' />
-                <DialogsItem name='Shukur' id='5' />
+                <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogsItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogsItem name={dialogsData[4].name} id={dialogsData[4].id} />
             </div>
 
             <div className={s.messages}>
-                <Messages message='Hey, yo!' />
-                <Messages message="Why anybody don't love me?" />
-                <Messages message='Inna Lillahi, Ina illahi Radzhiun' />
+                <Messages message={messagesData[0].message} />
+                <Messages message={messagesData[1].message} />
+                <Messages message={messagesData[2].message} />
             </div>
         </div>
 
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,20 @@ import Post from './Post/Post'
  
  const MyPosts = () =>{
 
+    let postsData = [
+        {img: 'https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        message: 'Hey, why nobody love me ?'},
+
+        {img: 'https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        message: "Hi everyone! It's already my second post!"},
+
+        {img: 'https://images.unsplash.com/photo-1587996833651-06a23343b15d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        message: "I love chocolate."},
+        
+        {img: 'https://images.unsplash.com/photo-1542736705-53f0131d1e98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+        message: "I am fun of React! "}
+    ]
+
     return(
 
         <div className={s.myPosts}>
@@ -15,17 +29,13 @@ import Post from './Post/Post'
 
 
             <div className="posts">
-                <Post img="https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                      message='Hey, why nobody love me ?' />
+                <Post img={postsData[0].img} message={postsData[0].message}/>
 
-                <Post img='https://images.unsplash.com/photo-1488740304459-45c4277e7daf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
-                      message=" Hi everyone! It's already my second post! " />
+                <Post img={postsData[1].img} message={postsData[1].message}/>
 
-                <Post img="https://images.unsplash.com/photo-1587996833651-06a23343b15d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGtpdHRlbnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                      message=" I love chocolate. " />
+                <Post img={postsData[2].img} message={postsData[2].message}/>
 
-                <Post img="https://images.unsplash.com/photo-1542736705-53f0131d1e98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                      message=" I am fun of React! " />
+                <Post img={postsData[3].img} message={postsData[3].message}/>
             </div>
             
         </div>
