a9a8526e7ee7535e16d9c08d17b99057104cbfb8
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-02-02 19:01:19 +0300

    feat: added textarea in dialogs component(also ability of fetching data from textarea)

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,31 +1,44 @@
 
+import React from 'react'
 import s from './Dialogs.module.css'
 import DialogsItem from './DialogsItem/DialogsItem'
 import Messages from './Messages/Messages'
 
 const Dialogs = (props) => {
 
+    // Рендеринг DialogsItem и Messages используя данные из state
+    let dialogsElements = props.state.dialogsData.map(item => <DialogsItem img={item.img} name={item.name} id={item.id} />)
+    let messagesElements = props.state.messagesData.map(item => <Messages message={item.message} id={item.id} />)
 
-    let dialogsElements = props.state.dialogsData.map( item => <DialogsItem img={item.img} name={item.name} id={item.id} /> )
-    let messagesElements = props.state.messagesData.map( item => <Messages message={item.message} id={item.id} /> )
+    // Получение данных из textarea
+    let textareaAnchor = React.createRef() // создаем и вешаем якорь на textarea чтобы потом его найти
+    let newMessage = () => {
+        const value = textareaAnchor.current.value
+        alert(value)
+    }
 
-
-    return(
+    return (
 
         <div className={s.dialogs}>
-            
+
             <div className={s.dialogsItem}>
                 <h2>DIALOGS</h2>
 
                 {dialogsElements}
-                
 
             </div>
 
             <div className={s.messages}>
 
-                {messagesElements}
-                
+                <div>
+                    {messagesElements}
+                </div>
+
+                <form action='#' className={s.newMessage}>
+                    <textarea ref={textareaAnchor} placeholder='your news...' rows="3"></textarea>
+                    <div className={s.controlls}> <button onClick={newMessage} className={s.btn} type="submit"> Send </button> </div>
+                </form>
+
             </div>
         </div>
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,12 +7,13 @@ const Header = () =>{
 
     <header className={s.header}>
       <div className={s.container}>
+      
         <img src='https://api.freelogodesign.org/assets/thumb/logo/8027670_400.png?t=637885647830000000' alt="Logo" />
       </div>
     </header> 
 
   );
-
+  
 }
 
 export default Header
