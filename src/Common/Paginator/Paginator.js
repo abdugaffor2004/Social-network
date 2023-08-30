40b7628ee9c6fa258cf12b584f02f1d3c06f13df
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2023-08-30 19:28:03 +0300

    feat: enhanced the paginator.

--- src/Common/Paginator/Paginator.js
+++ src/Common/Paginator/Paginator.js
@@ -1,8 +1,11 @@
-import s from "../../Components/FindUsers/FindUsers.module.css";
+import { useState } from "react";
+import s from "./Paginator.module.css"
+import {HiArrowLeft, HiArrowRight} from 'react-icons/hi'
 
 
 
-const Paginator = (props) =>{
+
+const Paginator = (props, {portionSize=10}) =>{
 
     let pagesCount = props.totalUsersCount/props.pageSize; //Сколько страниц получается
     let pages =[] //массив из цифр(номеров страниц)
@@ -15,16 +18,33 @@ const Paginator = (props) =>{
     };
 
 
+    const portionCount = Math.ceil(pagesCount / portionSize) // Число порций
+    const [portionNumber, setPortionNumber] = useState(1) // Номер порции
+    const infignum = (portionNumber - 1)* portionSize + 1 // нижняя граница форм-ла вычисления
+    const supremum = portionNumber * portionSize // верхняя граница форм-ла вычисления
+    
+
     return <div className={s.pagination}>
 
     {
-      pages.map( (page) =>{
-        return <button onClick={ () => setCurrentPageCaller(page)} className={ props.currentPage === page ? s.selectedPage : null}> {page} </button>
-      }) //Перебираем массив pages и отрисовываем кнопки пагинации, выделяем активную копку
+      portionNumber > 1 && <button className={s.prevNextPortionButton} onClick={ () => {setPortionNumber(portionNumber- 1); setCurrentPageCaller(infignum - portionSize) }}> <HiArrowLeft size={21}/> </button>
+      // Если номер порции больше 1 то показываем копку prev
     } 
+
+    {
+      pages.filter( (p) => p >= infignum && p <= supremum )
+      .map( (page) =>{
+        return <button onClick={ () => setCurrentPageCaller(page)} className={ (props.currentPage === page) ? s.selectedPage : null}> {page} </button>
+      }) //Перебираем массив pages и отрисовываем кнопки пагинации в отрезке от infignum до supremum, выделяем активную копку
+    } 
+
+    {
+      portionNumber <= portionCount && <button className={s.prevNextPortionButton} onClick={ () => {setPortionNumber(portionNumber+ 1); setCurrentPageCaller(infignum+portionSize)} }> <HiArrowRight size={21}/> </button>
+    }
+
 
 
   </div>
 }
 
-export default Paginator
+export default Paginator 
